6dc1b27ba7c54405206b76b389ab6fa5f96be985
jmk11 <z5160822@student.unsw.edu.au>
2020-10-06 20:31:46 +1100

    some refactoring

--- main.js
+++ main.js
@@ -2,14 +2,15 @@
 // !! todo: deal with some comments using \r to terminate lines????
 // wc itt hoo_ts: incorrect formatting - no x ie just (2.0) - should I accomodate that?
 // todo: Predictions removal case insensitive
-// todo: add var to declarations
 // all-results thing is missing some people - even if their comment wasn't right and they got 0 they should still be listed
 // todo: make the whole row in all-results lead to user update
 // todo: allow editing of the result input - or maybe change it to use 2 pages so you can just go back?
 // todo: encode/sanitise reddit content before putting in page - xss
+// todo: change so it doesn't all continue from one promise call
 
 // done: change toLowerCase() so I can put their original comment in the entry section
 // use innerText instead of innerHTML for xss reasons, use table insertCell + innerText instead of innerHTML
+// done: add var to declarations
 
 let results = []; // why did I make this global
 const multipliers = [2.0, 1.8, 1.6, 1.4, 1.2, 1.0, 1.0, 1.0];
@@ -29,23 +30,14 @@ form.addEventListener('submit', (event) => {
     for (let i = 0; i < cells.length; i++) {
         cells[i].innerText = results[i];
     }
-    // for (let i = 0; i < results.length; i++) {
-    //     results[i] = results[i].toLowerCase();
-    // }
-    // console.log(results[i]);
 
-    // 
-    let reddit = form.elements.reddit.value;
-    let apiURL = reddit + '/.json?depth=1'
+    let apiURL = form.elements.reddit.value + '/.json?depth=1';
     fetch(apiURL).then(function(resp) {
-        // catch network error
+        // todo: catch network error
         if (resp.status != 200) {
             alert('Error with reddit request.');
         } else {
             resp.json().then(function(json) {
-                // message = document.getElementsByClassName('reddit-response')[0];
-                // message.style.visibility = 'visible';
-                // alert(json[0]['kind']);
                 // raceName = json[0]['data']['children'][0]['data']['title'].split('] ')[1].split(' Predictions')[0]; // Predictions split should be case insensitive
                 let raceName = json[0]['data']['children'][0]['data']['title'].split('] ')[1].split(' -')[0].split('Predictions')[0]; // Predictions split should be case insensitive
                 form.style.display = 'none';
@@ -66,18 +58,25 @@ function makeElemsVisible() {
 
 // json: json of reddit comments. results: results array 1st-10th
 // only need top level comments
-function processResults(json, results) {
+function processResults(comments, results) {
     let rfl = {};
-    // let i = 0;
+    rfl = loadEntries(comments, rfl);
+    rfl = calculateScores(rfl, results);
+    const sortedUsernames = sortUsernames(rfl);
+    updateResults(rfl, sortedUsernames);
+    updateUserEntry(rfl, sortedUsernames[0], results);
+}
+
+function loadEntries(comments, rfl) {
     // multipliers = {"x2.0": "2.0", "x1.8": "1.8", "x1.6": "1.6", "x1.4": "1.4", "x1.2": "1.2", "x1.0": "1.0"};
-    for (let comment in json) {
-        let username = json[comment]['data']['author'];
-        let body = json[comment]['data']['body'];
+    const multStrs = ["x2.0)", "x1.8)", "x1.6)", "x1.4)", "x1.2)", "x1.0)", "x1.0)", "x1.0)"];
+    // multKeys = ["2.0", "1.8", "1.6", "1.4", "1.2", "1.0a", "1.0b", "1.0c"];
+    for (let comment of comments) {
+        let username = comment['data']['author'];
+        let body = comment['data']['body'];
         let entry = [];
         let lines = body.split("\n");
         let multIndex = 0;
-        const multStrs = ["x2.0)", "x1.8)", "x1.6)", "x1.4)", "x1.2)", "x1.0)", "x1.0)", "x1.0)"];
-        // multKeys = ["2.0", "1.8", "1.6", "1.4", "1.2", "1.0a", "1.0b", "1.0c"];
         for (let i = 0; i < lines.length && multIndex < multStrs.length; i++) {
             let rider = lines[i].split(multStrs[multIndex])[1];
             if (rider) {
@@ -116,41 +115,35 @@ function processResults(json, results) {
         // console.log(entry['x2.0']);
     }
     // console.log(rflEntries);
-    calculateScores(rfl, results);
+    return rfl;
 }
 
 function calculateScores(rfl, results) {
     // multipliers = {"2.0": 2.0, "1.8": 1.8, "1.6": 1.6, "1.4": 1.4, "1.2": 1.2, "1.0a": 1.0, "1.0b": 1.0, "1.0c": 1.0};
-    let order = []; // get users in sorted order somehow
-    let maxscore = 0; // temporary, until sorting is done
-
     for (let user of Object.keys(rfl)) {
-        console.log(rfl[user]['entry']);
+        // console.log(rfl[user]['entry']);
         let lcEntry = [];
         for (const rider of rfl[user]['entry']) {
             lcEntry.push(rider.toLowerCase());
         }
-        console.log(lcEntry);
+        // console.log(lcEntry);
         rfl[user]['score'] = 0;
         for (let i = 0; i < results.length; i++) {
-            let index = lcEntry.indexOf(results[i].toLowerCase()); // if repeated (breaks rfl ules), returns first occurence
+            let index = lcEntry.indexOf(results[i].toLowerCase()); // if repeated (breaks rfl rules), returns first occurence
             // repeating toLowerCase() call many times
             if (index != -1) {
                 // console.log(rfl[user]['entry'][index]);
                 rfl[user]['score'] += points[i]*multipliers[index];
             }
         }
-        console.log(user);
-        console.log(rfl[user]['score']);
+        // console.log(user);
+        // console.log(rfl[user]['score']);
 
         // for (const key of Object.keys(multipliers)) {
         //     if (rfl[user]['entry']['key']
         // }
     }
-    const sortedUsernames = sortUsernames(rfl);
-    console.log(sortedUsernames);
-    writeScores(rfl, sortedUsernames);
-    updateUserEntry(rfl, sortedUsernames[0], results);
+    return rfl;
 }
 
 function sortUsernames(rfl) {
@@ -161,27 +154,27 @@ function sortUsernames(rfl) {
     return usernames;
 }
 
-// not sorted yet
-function writeScores(rfl, usernames) {
+function updateResults(rfl, usernames) {
     let tbody = document.getElementsByClassName("allresults-tbody")[0];
     for (let i = 0; i < usernames.length; i++) {
         // let row = document.createElement("tr");
         // row.innerHTML = `<td>${i+1}</td><td>${usernames[i]}</td><td>${rfl[usernames[i]]['score'].toFixed(1)}</td>`; // xss
         let row = tbody.insertRow(i);
         row.insertCell(0).innerText = `${i+1}`;
-        row.insertCell(1).innerText = usernames[i];
+        let username = row.insertCell(1);
+        username.innerText = usernames[i];
         row.insertCell(2).innerText = rfl[usernames[i]]['score'].toFixed(1);
 
-        row.addEventListener('click', (event) => { // declare this as a separate function
+        username.addEventListener('click', (event) => { // closure for rfl
             let username = event.target.innerText; // event.srcElement
             console.log(this);
             updateUserEntry(rfl, username, results);
+            document.getElementsByClassName("user-results")[0].scrollIntoView();
             // window.scrollTo(window.scrollX, 0);
             // document.getElementsByClassName("user-score")[0].scrollIntoView();
             // let rect = document.getElementsByClassName("user-results")[0].getBoundingClientRect();
             // console.log(rect);
             // window.scrollTo({top: rect.top, behaviour: 'smooth'});
-            document.getElementsByClassName("user-results")[0].scrollIntoView();
         });
         tbody.appendChild(row);
     }
